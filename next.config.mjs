2235797156f5c2e7775a7d711125e1d0b2af904c
WangYiHui <wangyh265@gmail.com>
2025-04-29 00:46:39 +0800

    Add GitHub Pages deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/smartpath.gtihub.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/smartpath.gtihub.io/' : '',
 }
 
 export default nextConfig
